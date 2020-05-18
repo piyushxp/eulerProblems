cb768d1199d9be1a12dbfc7d44aa18bf9217bc7a
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-05-18 23:24:52 +0530

    This is problem#4

--- 4.largestPalindromeProduct/problem4.js
+++ 4.largestPalindromeProduct/problem4.js
@@ -2,4 +2,31 @@
 
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+function largestPalindromeProduct(digit) {
+  // We'll initialize "result" with a value, as we'll be using it later on
+  var result = 0;
+  var a;
+  var b;
+  if (digit == 2) {
+    a = 99;
+    b = 99;
+  } else {
+    a = 999;
+    b = 999;
+  }
 
+  for (var i = 1; i <= a; i++) {
+    for (var y = 1; y <= b; y++) {
+      var calc = i * y;
+      var calcString = calc.toString();
+      var calcStringReverse = calcString.split('').reverse().join('');
+      // This is the fundamental change necessary to get it working as intended
+      if (calcString === calcStringReverse && calc > result) {
+        result = calc;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(largestPalindromeProduct(9009));
